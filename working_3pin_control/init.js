f8a3727d91aefda1ad5bd287ceac74442e2c0619
navaneethmalingan <navaneethmalingan@gmail.com>
2017-06-10 16:59:04 +0530

    Final Code

--- working_3pin_control/init.js
+++ working_3pin_control/init.js
@@ -16,16 +16,25 @@ GPIO.set_mode(d2, GPIO.MODE_OUTPUT);
 
 
 MQTT.sub('control/n1', function(conn, topic, msg) {
-  
+
     let pub_topic = 'devices/' + Cfg.get('device.id');
     print(msg);
     let myJSON = JSON.parse(msg);
     print('mssage json ' , myJSON.status);
-    
+
     GPIO.write(myJSON.pin, myJSON.status);
 
     let ok = MQTT.pub(pub_topic, JSON.stringify({ pin: GPIO.read(d4)}), 1);
     print('Published:', ok ? 'yes' : 'no', 'topic:', pub_topic, 'pin status:', GPIO.read(d4));
     print('pin status: D1 : ', GPIO.read(d1), ' D2 : ', GPIO.read(d2), ' D4 : ', GPIO.read(d4));
+
+    /*
+HTTP.query({
+url: 'http://livemonitoring.info/node/fetch.php',
+//data: JSON.stringify({'status': GPIO.read(myJSON.pin),'pin': myJSON.pin,'node': 'n4'}),  // Optional. If set, POST is used
+success: function(body, full_http_msg) { print(body); },
+error: function(err) { print(err); },  // Optional
+});
+*/
 
 }, null);
